d80be3e0d64deb90f303ecb320a8f3ca9b803ab3
ngenne <42961385+ngenne@users.noreply.github.com>
2020-10-23 17:00:28 +0200

    Update folder_drawer.py
    Ability to categorize empty folders or not added

--- folder_drawer.py
+++ folder_drawer.py
@@ -28,8 +28,19 @@ def folder_drawer(path):
     df_sorted = df[df['folder'] == True].sort_values(by='id').set_index('id')
     df_sorted = pd.DataFrame(df_sorted)
     
+    df_sorted['empty'] = ''
+    
+    for each in df_sorted['path']:
+        if not os.listdir(each):
+            df_sorted['empty'][df_sorted['path'] == each] = True
+        else:
+            df_sorted['empty'][df_sorted['path'] == each] = False
+    
     nodes_names = df_sorted['name']
     nodes_names = pd.DataFrame(nodes_names)
+    
+    nodes_empty = df_sorted['empty']
+    nodes_empty = pd.DataFrame(nodes_empty)
     
     G = nx.Graph(directed=True)
     
@@ -39,9 +50,11 @@ def folder_drawer(path):
     
     g_nodes = list(G.nodes)
     nodes_names = nodes_names.loc[g_nodes]
+    nodes_empty = nodes_empty.loc[g_nodes]
     
     for node in G.nodes():
         G.nodes[node]['label'] = nodes_names['name'][node]
+        G.nodes[node]['empty'] = nodes_empty['empty'][node]
     
     nx.write_graphml(G, 'map_{}.graphml'.format(datetime.datetime.today().strftime("%Y-%m-%d--%H%M%S")))
 
