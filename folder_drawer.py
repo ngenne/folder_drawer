0d6ff4a5cd7e8079a2f0bfc731da8c58548ebda5
ngenne <42961385+ngenne@users.noreply.github.com>
2020-10-26 10:29:05 +0100

    Update folder_drawer.py
    Comments added

--- folder_drawer.py
+++ folder_drawer.py
@@ -15,19 +15,22 @@ args = parser.parse_args()
 def folder_drawer(path):
     path = args.Path
     if path.startswith(r'\\'):
-        path = (r'\\?\UNC' + path[1:])
+        path = (r'\\?\UNC' + path[1:]) # if UNC path this prefix is added
     else:
-        pass
-
+        pass # else it is a local path, so keep it as initial
+    
+    # Check if the path specified exists
     if not os.path.isdir(path):
         print('The path specified does not exist')
         sys.exit()
-        
-    df = folderstats.folderstats(path, ignore_hidden=True, absolute_paths=False)
     
+    # Retrieves the statistics of the browsed files
+    df = folderstats.folderstats(path, ignore_hidden=True, absolute_paths=False)
+    # Holds folders only and sort on the value 'id'.
     df_sorted = df[df['folder'] == True].sort_values(by='id').set_index('id')
     df_sorted = pd.DataFrame(df_sorted)
     
+    # Differentiation if folder is empty (True) or not (False)
     df_sorted['empty'] = ''
     
     for each in df_sorted['path']:
@@ -36,26 +39,33 @@ def folder_drawer(path):
         else:
             df_sorted.loc[df_sorted['path'] == each, ['empty']] = False
     
+    # Folder name retrieval to update network nodes and colors if folder is empty/full
+    # label (yEd)
     nodes_names = df_sorted['name']
     nodes_names = pd.DataFrame(nodes_names)
-    
+    # empty (yEd)
     nodes_empty = df_sorted['empty']
     nodes_empty = pd.DataFrame(nodes_empty)
     
+    # NetworkX Graph creation
     G = nx.Graph(directed=True)
     
+    # Creation of network edges from the 'parent' and 'id' variables of df_sorted
     for i, row in df_sorted.iterrows():
         if row.parent:
             G.add_edge(i, row.parent)
     
+    # Recovery of the node index and reindexing of the dataframe "nodes_names" with "g_nodes"
     g_nodes = list(G.nodes)
     nodes_names = nodes_names.loc[g_nodes]
     nodes_empty = nodes_empty.loc[g_nodes]
     
+    # Adding "label" and "empty" properties for each node to ease properties mapping in yEd
     for node in G.nodes():
         G.nodes[node]['label'] = nodes_names['name'][node]
         G.nodes[node]['empty'] = nodes_empty['empty'][node]
     
+    # Saving the graph in XML format
     nx.write_graphml(G, 'map_{}.graphml'.format(datetime.datetime.today().strftime("%Y-%m-%d--%H%M%S")))
 
     print('Mapping complete')
